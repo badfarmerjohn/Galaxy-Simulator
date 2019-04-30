bdcf7796f7c9d392fcf0679adff614878138a1a3
Eric <ejyzhang@berkeley.edu>
2019-04-30 13:50:31 -0700

    Started GPU Particle Manager

---
+++ Assets/Scripts/Particle Managers/GPUParticleManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class GPUParticleManager : MonoBehaviour
+{
+    public VisualEffect visualEffect;
+
+    //VFXEventAttribute spawn_attr;
+
+    Texture2D new_point_positions;
+    Texture2D new_point_scales;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawn_attr = visualEffect.CreateVFXEventAttribute();
+
+        new_point_positions = new Texture2D(SystemInfo.maxTextureSize, 1, TextureFormat.RGBAFloat, false);
+        new_point_scales = new Texture2D(SystemInfo.maxTextureSize, 1, TextureFormat.RGBAFloat, false);
+    }
+
+    public int i = 0;
+    public void AddPoints(Vector3[] newPositions)
+    {
+        NativeArray<Color> rawData = new_point_positions.GetRawTextureData<Color>();
+        for (int begin = 0; begin < newPositions.Length; begin += SystemInfo.maxTextureSize)
+        {
+            int end = Mathf.Min(begin + SystemInfo.maxTextureSize, newPositions.Length);
+            for (int j = begin, k = 0; j < end; j++, k++)
+            {
+                rawData[k] = new Color(newPositions[j].x, newPositions[j].y, newPositions[j].z);
+            }
+            new_point_positions.Apply();
+            visualEffect.SetInt("num_new_particles", end - begin);
+            visualEffect.SetTexture("new_particle_positions", new_point_positions);
+            visualEffect.SetVector3("offset", i * Vector3.one);
+            visualEffect.SendEvent("AddParticles");
+            Debug.Log("Adding " + (end - begin));
+        }
+        i += 1;
+    }
+}
---
+++ Assets/Scripts/Particle Managers/GPUParticleTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUParticleTest : MonoBehaviour
+{
+    GPUParticleManager manager;
+
+    int amount = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GetComponent<GPUParticleManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3[] particles = new Vector3[amount];
+            for (int i = 0; i < particles.Length; i++)
+            {
+                particles[i] = 1 * (new Vector3(Random.value, Random.value, Random.value));
+            }
+            manager.AddPoints(particles);
+            //amount += 10;
+        }
+    }
+}
