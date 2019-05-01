4319a6ba691c0a7a2f12797e3a5871462c3c9ff4
Ryan Meyer <ryanmeyerstuff@gmail.com>
2019-04-30 23:21:20 -0700

    skeleton data structure for physical particle

---
+++ Assets/DataStructures/PhysicalParticle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalParticle
+{
+  float size;
+  float mass;
+  Vector3 position;
+  Color color;
+
+}
