df2824558e2fd8b62066268b490edf7d22d4614c
Ryan Meyer <ryanmeyerstuff@gmail.com>
2019-05-01 00:53:33 -0700

    naive force calculation and force application

--- Assets/Scripts/PhysicalParticleSimulator.cs
+++ Assets/Scripts/PhysicalParticleSimulator.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PhysicalParticleSimulator
 {
-  public const double GRAVITATIONAL_CONSTANT = 1.0;
+  public const float GRAVITATIONAL_CONSTANT = 1.0;
   public PhysicalParticle[] physical_particles;
 
   void ApplyForce(uint index, Vector3 total_force, float dt)
   {
-    physical_particles[i].position += physical_particles[i].velocity * dt;
-    physical_particles[i].velocity += total_force / physical_particles[i].mass * dt;
+    physical_particles[index].position += physical_particles[index].velocity * dt;
+    physical_particles[index].velocity += total_force / physical_particles[index].mass * dt;
   }
 
   Vector3 NaiveForceOnParticle(uint index)
@@ -20,8 +20,10 @@ public class PhysicalParticleSimulator
     for (int j = 0; j < index; ++j)
     {
       Vector3 position_difference = p_i.position - physical_particles[j].position;
-      force += GRAVITATIONAL_CONSTANT * physical_particles[j].mass * (position_difference / position_difference.mag);
+      float pd_cubed = position_difference.magnitude * position_difference.magnitude * position_difference.magnitude;
+      force += GRAVITATIONAL_CONSTANT * physical_particles[j].mass * (position_difference / pd_cubed);
     }
+    return force;
   }
 
 }
